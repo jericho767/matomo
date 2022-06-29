b332a2175f89e3d37955049536d836155bd062de
Jericho Gimoros <jericho.6367@gmail.com>
2022-06-29 13:55:29 +1200

    Add docker setup files

---
+++ docker/nginx/scripts/configure-development.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+mkdir -p /etc/nginx/ssl
+sed -i "s/API_DOMAIN/$API_DOMAIN/g" /etc/nginx/conf.d/default.conf
+openssl dhparam -out /etc/nginx/ssl/dhparam.pem 2048
+
+# Create Self-signed SSL Cert for API Domain
+if [ ! -z "$API_DOMAIN" ]
+then
+openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
+  -keyout /etc/nginx/ssl/$API_DOMAIN.key -out /etc/nginx/ssl/$API_DOMAIN.cert -extensions san -config \
+  <(echo "[req]";
+    echo distinguished_name=req;
+    echo "[san]";
+    echo subjectAltName=DNS:$API_DOMAIN,IP:10.0.0.1
+    ) \
+  -subj /CN=$API_DOMAIN
+fi
